070e5ec5f67726b6ff359c2119a9a84ef63b3a81
psdoood <aydeniswright@gmail.com>
2024-09-08 20:33:36 -0600

    Got a basic filesystem window working

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,8 +2,8 @@
 #include "imgui.h"
 #include <string>
 #include <vector>
-#include <string>
-#include <iostream>
+#include <filesystem>
+#include <algorithm>
 
 #include "Crypto.h"
 #include "Steganography.h"
@@ -12,25 +12,59 @@ namespace appUI
 {
     static Steganography steganoObj;
     static Crypto cryptoObj;
-    static std::string inImagePath = "Drop Your Image Here";
+    static std::string inImagePath = "Input Image Shown Here";
     static std::string outImagePath = "Altered Image Shown Here";
     static GLuint inImageTexture = 0;
     static GLuint outImageTexture = 0; 
     static char masterKey[256] = "";
     static char data[1024] = "";//adjust
     static char extractedData[1024] = "";//adjust
+    static bool showFileExplorer = false;
+    static std::string currentPath = std::filesystem::current_path().string();
+    static std::string lastLoadedPath = "";
+    static std::vector<std::string> files;
     
+    
+    //************************************************************************************************************//
+
     GLuint loadTexture(const std::string path){
         Image loadedImg = steganoObj.loadAndConvert(path);
+
         GLuint image_texture;
         glGenTextures(1, &image_texture);
         glBindTexture(GL_TEXTURE_2D, image_texture);
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, loadedImg.width, loadedImg.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, loadedImg.data);
+
         steganoObj.cleanImage(loadedImg);
         return image_texture;
     }
+
+    //************************************************************************************************************//
+
+    void updateFiles(){
+        files.clear();
+        for(const auto& item : std::filesystem::directory_iterator(currentPath)){
+            if(item.is_directory() || isImageFile(item.path().extension().string())) {
+            files.push_back(item.path().filename().string());
+            }
+        }
+        std::sort(files.begin(), files.end());
+    }   
+
+    //************************************************************************************************************//
+
+    bool isImageFile(const std::string& extension){
+        std::vector<std::string> extensions = {".jpeg", ".jpg", ".bmp", ".png", ".tga", ".psd"};
+        return std::find(extensions.begin(), extensions.end(), extension) != extensions.end();
+    }
+
+    //************************************************************************************************************//
 
     void renderUI(){
         ImGuiWindowFlags window_flags = 0;
@@ -68,12 +102,22 @@ namespace appUI
         if(inImageTexture == 0){
             ImGui::Button(inImagePath.c_str(), ImVec2(halfWidth, 400));
         } else{
-            ImGui::Image((void*)(intptr_t)inImageTexture, ImVec2(halfWidth, 400));
+            float aspectRatio = 0.0f;
+            ImVec2 imageSize(halfWidth, halfWidth);
+            if(inImagePath != lastLoadedPath){
+                Image loadedImg = steganoObj.loadAndConvert(inImagePath);
+                aspectRatio = (float)loadedImg.width / loadedImg.height;
+                imageSize.x = halfWidth;
+                imageSize.y = halfWidth / aspectRatio;
+                steganoObj.cleanImage(loadedImg);
+                lastLoadedPath = inImagePath;
+            }
+            ImGui::Image((void*)(intptr_t)inImageTexture, imageSize);
         }
         ImGui::EndGroup();
 
         ImGui::SameLine(0, ImGui::GetStyle().ItemSpacing.x);
-        
+
 
         //Output image section
         ImGui::BeginGroup();
@@ -88,7 +132,42 @@ namespace appUI
             ImGui::Image((void*)(intptr_t)outImageTexture, ImVec2(halfWidth, 400));
         }
         ImGui::EndGroup();
-        
+
+
+        //Section for file explorer
+        if(ImGui::Button("Open File Explorer")){
+            showFileExplorer = true;
+            updateFiles();
+        }
+        if(showFileExplorer){
+            ImGui::Begin("File Explorer", &showFileExplorer);
+            
+            if(ImGui::Button("<- Go Back")){
+                currentPath = std::filesystem::path(currentPath).parent_path().string();
+                updateFiles();
+            }
+
+            for(const auto& file : files){
+                if(ImGui::Selectable(file.c_str())){
+                    std::string fullPath = (std::filesystem::path(currentPath) / file).string();
+                    bool isDirectory = std::filesystem::is_directory(fullPath);
+                    if(isDirectory){
+                        currentPath = fullPath;
+                        updateFiles();
+                    }else{
+                        inImagePath = fullPath;
+                        if(inImageTexture != 0){
+                            glDeleteTextures(1, &inImageTexture);
+                            inImageTexture = 0;
+                        }
+                        inImageTexture = loadTexture(inImagePath);
+                        showFileExplorer = false;
+                    }
+                }
+            }
+            ImGui::End();
+        }
+
 
         ImGui::InputText("Master Key", masterKey, AES_KEYLEN);
         ImGui::InputText("Data to Hide", data, IM_ARRAYSIZE(data)); //adjust buffer size at some point
--- src/Application.h
+++ src/Application.h
@@ -5,10 +5,13 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
+#include <string>
 
 namespace appUI
 {
         GLuint loadTexture(const char* path);
+        void updateFiles();
+        bool isImageFile(const std::string& extension);
         void renderUI();
 }
 
